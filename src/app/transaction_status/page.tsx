633cdcdf58fd1fe1391923b85ab33d1f1a91df03
congtrung6391 <congtrung6391@gmail.com>
2023-04-04 07:47:14 +0700

    debug

--- src/app/transaction_status/page.tsx
+++ src/app/transaction_status/page.tsx
@@ -20,13 +20,13 @@ const Page = async ({ searchParams }: any) => {
 
   // const tmnCode = process.env.VNPAY_TMNCODE;
   // const secretKey = process.env.VNPAY_HASHSECRET as string;
-  const secretKey = ''; // process.env.NEXT_PUBLIC_VNP_HASHSECRET;
+  const secretKey = process.env.NEXT_PUBLIC_VNP_HASHSECRET;
 
   const signData = querystring.stringify(vnp_Params, { encode: false });
   const hmac = crypto.createHmac('sha512', secretKey || '');
   const signed = hmac.update(new Buffer(signData, 'utf-8')).digest('hex');
 
-  if (secureHash === signed) {
+  if (secureHash !== signed) {
     //Kiem tra xem du lieu trong db co hop le hay khong va thong bao ket qua
     if (vnp_Params.vnp_ResponseCode === '24') {
       return redirect('/');
